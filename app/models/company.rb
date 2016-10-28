101286f6fafb221f43b8e26ae5c7b5b0306be904
Ali <aqattan@yahoo.com>
2016-10-28 15:20:59 +0000

    test2

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,4 @@
 class Company < ActiveRecord::Base
     validates_presence_of:Company_name, :message => ":Please Enter a valid company name"
+    has_many :employees
 end
--- db/migrate/20161028132100_create_companies.rb
+++ db/migrate/20161028132100_create_companies.rb
@@ -2,7 +2,6 @@ class CreateCompanies < ActiveRecord::Migration
   def change
     create_table :companies do |t|
       t.string :Company_name
-      t.has_many:employees 
       t.timestamps null: false
     end
   end
